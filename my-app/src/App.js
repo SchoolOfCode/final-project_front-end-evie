b0ed73d5b0c4f8cc2cedc17480bd31ab027f0d07
soting <sotingip@gmail.com>
2022-07-25 15:46:54 +0100

    Maps component added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,22 @@
 import * as React from 'react';
-import Map from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
 import './App.css';
+import Maps from './components/Map/Maps';
 
 
-mapboxgl.accessToken ='pk.eyJ1IjoibmVlbW9kYWIiLCJhIjoiY2w2MGpqNDVlMWtlMzNscnRzamk4MmJ6diJ9.I_R2v2UXU5W1-25uPNwePQ'
-
-
+mapboxgl.accessToken=process.env.Key
 
 
 function App() {
+
+
+
+
+
   return (
     <div className="App">
-        <Map
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }}
-      style={{width: 600, height: 400}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+        <Maps/>
     </div>
   );
 }
