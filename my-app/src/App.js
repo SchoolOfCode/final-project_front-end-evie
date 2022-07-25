aa38aa2768fcb3d16b9e5372650b5ac01d7d438b
NeemoDab <neemodab@gmail.com>
2022-07-25 11:00:52 +0100

    playing around with map API, map showing on screen

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Map from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+// import logo from './logo.svg';
+import './App.css';
+mapboxgl.accessToken ='pk.eyJ1IjoibmVlbW9kYWIiLCJhIjoiY2w2MGpqNDVlMWtlMzNscnRzamk4MmJ6diJ9.I_R2v2UXU5W1-25uPNwePQ'
+function App() {
+  return (
+    <div className="App">
+        <Map
+      initialViewState={{
+        longitude: -122.4,
+        latitude: 37.8,
+        zoom: 14
+      }}
+      style={{width: 600, height: 400}}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
+    </div>
+  );
+}
+
+export default App;
