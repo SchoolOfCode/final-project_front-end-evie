e01ef4620f312f531e463284664cf8f598eddf9d
Katie Walters <katie_walters@live.co.uk>
2022-07-28 16:04:30 +0100

    added markers component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-load
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
 //import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
-
+import Mapp from '../Markers/Markers'
 
 
 
@@ -13,45 +13,46 @@ mapboxgl.accessToken='pk.eyJ1IjoibmVlbW9kYWIiLCJhIjoiY2w2MGpqNDVlMWtlMzNscnRzamk
 
 
 function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-1.898575);
-  const [lat, setLat] = useState(52.489471);
-  const [zoom, setZoom] = useState(9);
+//   const mapContainer = useRef(null);
+//   const map = useRef(null);
+//   const [lng, setLng] = useState(-1.898575);
+//   const [lat, setLat] = useState(52.489471);
+//   const [zoom, setZoom] = useState(9);
 
-useEffect(() => {
-if (map.current) return; // initialize map only once
-map.current = new mapboxgl.Map({
-container: mapContainer.current,
-style: 'mapbox://styles/mapbox/streets-v11',
-center: [lng, lat],
-zoom: zoom
-});
-map.current.addControl(
-        new MapboxDirections({
-          accessToken: mapboxgl.accessToken
-        }),
-        'top-right'
-      );
-});
+// useEffect(() => {
+// if (map.current) return; // initialize map only once
+// map.current = new mapboxgl.Map({
+// container: mapContainer.current,
+// style: 'mapbox://styles/mapbox/streets-v11',
+// center: [lng, lat],
+// zoom: zoom
+// });
+// map.current.addControl(
+//         new MapboxDirections({
+//           accessToken: mapboxgl.accessToken
+//         }),
+//         'top-right'
+//       );
+
+// });
 
 
-//Store new coordinates that you get when a user interacts with the map
-useEffect(() => {
-if (!map.current) return; // wait for map to initialize
-map.current.on('move', () => {
-setLng(map.current.getCenter().lng.toFixed(4));
-setLat(map.current.getCenter().lat.toFixed(4));
-setZoom(map.current.getZoom().toFixed(2));
-});
-});
+// //Store new coordinates that you get when a user interacts with the map
+// useEffect(() => {
+// if (!map.current) return; // wait for map to initialize
+// map.current.on('move', () => {
+// setLng(map.current.getCenter().lng.toFixed(4));
+// setLat(map.current.getCenter().lat.toFixed(4));
+// setZoom(map.current.getZoom().toFixed(2));
+// });
+// });
 
 
   
 
   return (
     <>
-   <div>
+   {/* <div>
     <div className="sidebar">
       Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
     </div>
@@ -59,7 +60,8 @@ setZoom(map.current.getZoom().toFixed(2));
   </div>
   <div>
 
-  </div>
+  </div> */}
+  <Mapp />
   </>
 
   );
