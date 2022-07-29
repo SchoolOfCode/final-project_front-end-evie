eeeb51272e74bcea20042e56aeb750a405b266f5
Katie Walters <katie_walters@live.co.uk>
2022-07-29 10:01:38 +0100

    markers working on correct map!

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,54 +4,84 @@ import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-load
 import 'mapbox-gl/dist/mapbox-gl.css';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
 import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
-import Mapp from '../Markers/Markers'
+// import Mapp from '../Markers/Markers'
 
 
 
 mapboxgl.accessToken='pk.eyJ1IjoibmVlbW9kYWIiLCJhIjoiY2w2MGpqNDVlMWtlMzNscnRzamk4MmJ6diJ9.I_R2v2UXU5W1-25uPNwePQ';
 
+// Sample data 
+const data = [
+	{
+		"location": "Manhattan Ave & Norman Ave at NE corner",
+		"city": "Brooklyn",
+		"state": "New York",
+		"coordinates": [-73.9516030004786,40.72557300071668],
+	},
+	{
+		"location": "6th Ave & 42nd St at NW corner",
+		"city": "Manhattan",
+		"state": "New York",
+		"coordinates": [-73.98393399979334,40.75533300052329],
+	},
+	{
+		"location": "Essex St & Delancey St at SE corner",
+		"city": "Manhattan",
+		"state": "New York",
+		"coordinates": [-73.9882730001973,40.718207001246554],
+	}
+]
 
 function App() {
-//   const mapContainer = useRef(null);
-//   const map = useRef(null);
-//   const [lng, setLng] = useState(-1.898575);
-//   const [lat, setLat] = useState(52.489471);
-//   const [zoom, setZoom] = useState(9);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-1.898575);
+  const [lat, setLat] = useState(52.489471);
+  const [zoom, setZoom] = useState(9);
 
-// useEffect(() => {
-// if (map.current) return; // initialize map only once
-// map.current = new mapboxgl.Map({
-// container: mapContainer.current,
-// style: 'mapbox://styles/mapbox/streets-v11',
-// center: [lng, lat],
-// zoom: zoom
-// });
-// map.current.addControl(
-//         new MapboxDirections({
-//           accessToken: mapboxgl.accessToken
-//         }),
-//         'top-right'
-//       );
+useEffect(() => {
+if (map.current) return; // initialize map only once
+map.current = new mapboxgl.Map({
+container: mapContainer.current,
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [lng, lat],
+zoom: zoom
+});
+map.current.addControl(
+        new MapboxDirections({
+          accessToken: mapboxgl.accessToken
+        }),
+        'top-right'
+      );
 
-// });
+      data.forEach((location) => {
+        console.log(location)
+        var marker = new mapboxgl.Marker()
+                .setLngLat(location.coordinates)
+                .setPopup(new mapboxgl.Popup({ offset: 30 })
+                .setHTML('<h4>' + location.city + '</h4>' + location.location))
+                .addTo(map.current);
+  
+      })
+});
+		
 
-
-// //Store new coordinates that you get when a user interacts with the map
-// useEffect(() => {
-// if (!map.current) return; // wait for map to initialize
-// map.current.on('move', () => {
-// setLng(map.current.getCenter().lng.toFixed(4));
-// setLat(map.current.getCenter().lat.toFixed(4));
-// setZoom(map.current.getZoom().toFixed(2));
-// });
-// });
+//Store new coordinates that you get when a user interacts with the map
+useEffect(() => {
+if (!map.current) return; // wait for map to initialize
+map.current.on('move', () => {
+setLng(map.current.getCenter().lng.toFixed(4));
+setLat(map.current.getCenter().lat.toFixed(4));
+setZoom(map.current.getZoom().toFixed(2));
+});
+});
 
 
   
 
   return (
     <>
-   {/* <div>
+  <div>
     <div className="sidebar">
       Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
     </div>
@@ -59,8 +89,8 @@ function App() {
   </div>
   <div>
 
-  </div> */}
-  <Mapp />
+  </div>
+  {/* <Mapp /> */}
   </>
 
   );
