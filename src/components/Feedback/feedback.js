23d4acde2c0f1ed32c21700b41b6008e4a58beac
NeemoDab <neemodab@gmail.com>
2022-08-12 11:40:59 +0100

    fixing netlify issue
    Co-authored-by: Katie Walters katie_walters@live.co.uk
    Co-authored-by: Omar Labban labban.omar@gmail.com
    Co-authored-by: NeemoDab neemodab@gmail.com
    Co-authored-by: Abdul-Aziz Farah abdul-aziz_88@hotmail.com
    Co-authored-by: So Ting Angela Tsang sotingip@gmail.com

--- src/components/Feedback/feedback.js
+++ src/components/Feedback/feedback.js
@@ -112,6 +112,7 @@ function Feedback() {
           </div>
           <br />
           Charge point:
+          heloo im working well im trying
           <textarea placeholder="" className="textarea" />
           <br />
           Model of charger:
